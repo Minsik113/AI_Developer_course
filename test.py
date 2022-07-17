ef338ae1be6eb2d8563d54f7584613863dde275a
minsik113 <hyunicho62@gmail.com>
2022-07-17 22:00:44 +0900

    python_DataStructure_PythonicCode

---
+++ test.py
@@ -0,0 +1,11 @@
+def test_typehint_example(x:int, y:int):
+    """_summary_
+
+    Args:
+        x (int): _description_
+        y (int): _description_
+
+    Returns:
+        _type_: _description_
+    """    
+    return x + y
